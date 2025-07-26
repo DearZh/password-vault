8a22bf8dd13f4217e7f4b870eb3b0e984aaa811c
zhihao.zhao <zhihao.zhao@ingeek.com>
2025-07-26 17:20:47 +0800

    升级 Next.js 到 15.3.4 版本并更新相关配置

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   // 防止构建追踪导致的循环依赖问题
-  experimental: {
-    serverComponentsExternalPackages: ['sharp', 'to-ico'],
-  },
+  serverExternalPackages: ['sharp', 'to-ico'],
 };
 
 module.exports = nextConfig;

e7d2c75a76d741e8f5744d546ae71540dc6fb555
zhihao.zhao <zhihao.zhao@ingeek.com>
2025-07-28 17:05:03 +0800

    Clarity

--- app/layout.js
+++ app/layout.js
@@ -65,6 +65,19 @@ export default function RootLayout({ children }) {
         <meta name="keywords" content={metadata.keywords} />
         <meta name="author" content="密码保险箱团队" />
         
+        {/* Microsoft Clarity 跟踪代码 */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "slw7zlxiat");
+            `,
+          }}
+        />
+        
         {/* Structured Data for SEO */}
         <script type="application/ld+json" dangerouslySetInnerHTML={{
           __html: JSON.stringify({
